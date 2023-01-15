48bebcd0da19eb3fff261861c10bb198ace3af94
MichelleOffiong <offiongmichelle283@gmail.com>
2023-01-15 02:37:51 +0000

    new

---
+++ 0x04-python-more_data_structures/103-tests.py
@@ -0,0 +1,50 @@
+import ctypes
+
+lib = ctypes.CDLL('./libPython.so')
+lib.print_python_list.argtypes = [ctypes.py_object]
+lib.print_python_bytes.argtypes = [ctypes.py_object]
+s = b"Hello"
+lib.print_python_bytes(s);
+b = b'\xff\xf8\x00\x00\x00\x00\x00\x00';
+lib.print_python_bytes(b);
+b = b'What does the \'b\' character do in front of a string literal?';
+lib.print_python_bytes(b);
+l = [b'Hello', b'World']
+lib.print_python_list(l)
+del l[1]
+lib.print_python_list(l)
+l = l + [4, 5, 6.0, (9, 8), [9, 8, 1024], b"Holberton", "Betty"]
+lib.print_python_list(l)
+l = []
+lib.print_python_list(l)
+l.append(0)
+lib.print_python_list(l)
+l.append(1)
+l.append(2)
+l.append(3)
+l.append(4)
+lib.print_python_list(l)
+l.pop()
+lib.print_python_list(l)
+l = ["Holberton"]
+lib.print_python_list(l)
+lib.print_python_bytes(l);
+julien@ubuntu:~/CPython$ python3 103-tests.py
+[.] bytes object info
+  size: 5
+  trying string: Hello
+  first 6 bytes: 48 65 6c 6c 6f 00
+[.] bytes object info
+  size: 8
+  trying string: ï
+  first 9 bytes: ff f8 00 00 00 00 00 00 00
+[.] bytes object info
+  size: 60
+  trying string: What does the 'b' character do in front of a string literal?
+  first 10 bytes: 57 68 61 74 20 64 6f 65 73 20
+[*] Python list info
+[*] Size of the Python List = 2
+[*] Allocated = 2
+Element 0: bytes
+[.] bytes object info
+  
